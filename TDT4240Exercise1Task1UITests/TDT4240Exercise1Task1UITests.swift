8baa70fcc0fc800ec3bfbda08b854cda65811950
Kristoffer Larsen <kristoffer@larsen.so>
2016-02-06 13:00:03 +0100

    Initial Commit

---
+++ TDT4240Exercise1Task1UITests/TDT4240Exercise1Task1UITests.swift
@@ -0,0 +1,36 @@
+//
+//  TDT4240Exercise1Task1UITests.swift
+//  TDT4240Exercise1Task1UITests
+//
+//  Created by Kristoffer Larsen on 06.02.16.
+//  Copyright © 2016 Kristoffer Larsen. All rights reserved.
+//
+
+import XCTest
+
+class TDT4240Exercise1Task1UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
